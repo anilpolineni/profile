14adce5a7b2f95f59f54ca98d47eb719c41c687c
Anil <anilpolineni09@gmail.com>
2020-04-26 20:44:57 +0530

    Updates

---
+++ precache-manifest.a08801a1747064cf88f1872e32362b65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd2a60b05c9ca0fa1bbe6f6887b3909",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "e94caac3c6f6c0bc6201",
+    "url": "/profile/static/css/main.18259607.chunk.css"
+  },
+  {
+    "revision": "f47dfcac8531ead21e9c",
+    "url": "/profile/static/js/2.4d557a2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/profile/static/js/2.4d557a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94caac3c6f6c0bc6201",
+    "url": "/profile/static/js/main.a242bcd4.chunk.js"
+  },
+  {
+    "revision": "bd8fcd7b486537810a7e",
+    "url": "/profile/static/js/runtime-main.2aaa4f65.js"
+  }
+]);
